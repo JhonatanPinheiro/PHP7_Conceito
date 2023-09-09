a43abbe257867d8806d20f362ff2ab15d76b7a88
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2023-09-09 19:44:26 -0300

    Finalizando atividade: 30. Integração HTML

---
+++ basico/html.php
@@ -0,0 +1,16 @@
+<div class="title">INTEGRAÇÃO HTML</div>
+<hr><br>
+
+<h1>
+    <?php
+    echo 'Olá';
+    echo '<small>';
+    echo ' Mundo!';
+    echo '</smal>';
+    ?>
+</h1>
+<?= "<div> Outra forma de me 'Expressar'! </div>" ?>
+<br>
+<div>
+    <button> <?= "Legal" ?></button>
+</div>
