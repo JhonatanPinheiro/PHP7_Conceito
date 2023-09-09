de5874ec2e988f197b6bceca2a49a37bbd80ac75
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2023-09-09 17:56:12 -0300

    Finalizando a primeira atividade do exercicio: 22

--- basico/ola.php
+++ basico/ola.php
@@ -1,9 +1,14 @@
 <div class="title">Olá PHP</div>
-<hr>
+<br>
 <?php
-
-echo 'Olá';
-echo '<br>';
+echo 'Olá ';
 echo 'Mundo!';
-
 ?>
+
+<h2>Exemplo </h2>
+<?= "Outra forma de me 'expressar'!" ?> &nbsp;
+<?= (2 * 3) + 3 ?>
+
+<?= "<br><br>Esse comando irá mostrar sobre as configurações da linguagem PHP7" ?>;
+<?php
+phpinfo();
