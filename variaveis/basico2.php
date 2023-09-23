4014b68841eabdf892aa647609f0fc7d22242816
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2023-09-23 16:43:26 -0300

    55. Desafio Equação

---
+++ variaveis/basico2.php
@@ -0,0 +1,18 @@
+<div class="title">Variáveis Básicos </div>
+<hr><br>
+<?php
+
+
+# Deve ter um padrão para declarar variável . Cada empresa adquir de forma 
+
+#Mas em algumas questão, podemos declarar variáveis das seguintes formas 
+$var = 1;
+$var_month = 1;
+$var_4month = 4;
+echo '<br>';
+var_dump($_SERVER); # Dentro dessa função do PHP tem muitoas dados guardados , ele é um array com 32 elementos
+echo '<br>';
+#https://www.php.net/manual/pt_BR/reserved.variables.server.php
+
+
+?>
