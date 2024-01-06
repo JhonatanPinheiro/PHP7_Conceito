49f5541c2ae1199c235d1431a9fad2157f72c868
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-01-06 17:42:49 -0300

    Finazando a branch 58.Interpolação

--- variaveis/interpolacao.php
+++ variaveis/interpolacao.php
@@ -1,7 +1,17 @@
-<div class="title"> INTERPOLAÇÃO </div>
-<hr><br
-
+<div class="title"> Interpolação </div>
+<hr><br 
 <?php
+$numero = 10;
+echo $numero;
+echo "<br> $numero";
+
+$texto = "A sua nota é: $numero";
+echo "<br>$texto";
+
+$objeto = 'caneta';
+echo "<br>Eu tenho 5 $objeto";
+echo "<br>Eu tenho 5 {$objeto}s.";
+
 
 
 
