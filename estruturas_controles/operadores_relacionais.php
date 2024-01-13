94d3b4735761f17d2b61c700496f30c2d2fc7afe
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-01-13 17:54:11 -0300

    Commit da aula 71. Operadores Relacionais #01

--- estruturas_controles/operadores_relacionais.php
+++ estruturas_controles/operadores_relacionais.php
@@ -24,4 +24,16 @@ echo var_dump(!false);
 echo '<br>';
 echo var_dump(!true);
 echo '<br>';
+echo '_____________________________________';
+echo '<br>';
+
+$idade = 65;
+if($idade < 18){
+    echo "Menor de idade = $idade<br>";
+} else if($idade  < 65){
+    echo "Adulto =$idade<br>";
+}else {
+    echo"Terceira idade = $idade <br>";
+}
+
 ?>
