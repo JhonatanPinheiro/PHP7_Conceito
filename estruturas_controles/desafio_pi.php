3276b70af63de01569494aae811b9fae410dc215
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-01-13 19:49:28 -0300

    Desafio de PI - concluído

--- estruturas_controles/desafio_pi.php
+++ estruturas_controles/desafio_pi.php
@@ -1,19 +1,23 @@
 <div class="title"> Desafio de PI </div>
 <hr><br> 
 <?php
+ 
+ #Acertando apenas 4 primeiros numeros do pi já irá dar como correto , idependente se colocar depois numeros equivocados pois está na tonerância
 
 $valor_pi_correto = pi();
-$valor_pi_usuario = 3.1415926535898;
+$valor_pi_usuario = 3.1415;
 
 echo "$valor_pi_correto";
 echo '<br>';
 
-echo "{$valor_pi_correto} ";
-
 if($valor_pi_usuario >= 3.141 && $valor_pi_usuario <= 3.1415926535898){
-    echo"<p class='correct'>It's correct!</p>";
+    echo"<p class='correct'>It's correct!</p> <br>";
+    echo"Você informou: {$valor_pi_usuario} <br>";
+    echo"Valor de PI é: {$valor_pi_correto} <br>";
 }else{
     echo "<p class='wrong'>It's not correct!</p>";
+    echo"Você informou: {$valor_pi_usuario} <br>";
+    echo"Valor de PI é: {$valor_pi_correto} <br>";
 }
 
 ?>
