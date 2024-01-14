5bc8447f355e812a433ab02faca1e1370859106d
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-01-14 12:08:03 -0300

    77- GET/POST

--- index.php
+++ index.php
@@ -65,6 +65,7 @@
                     <li><a href="exercicio.php?dir=estruturas_controles&file=desafio_pi"> Desafio 01 - PI </a></li>
                     <li><a href="exercicio.php?dir=estruturas_controles&file=operadores_logicos_01"> Operadores Lógicos - 01 </a></li>
                     <li><a href="exercicio.php?dir=estruturas_controles&file=operadores_logicos_02"> Operadores Lógicos - 02 </a></li>
+                    <li><a href="exercicio.php?dir=estruturas_controles&file=GET_POST"> Diferença : GET e POST </a></li>
                 </ul>
             </div>
             <div class="modulo mod-5">
