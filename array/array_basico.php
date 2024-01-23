e5f957da36a2e59eb0af59bcb1dffff0a1b532ec
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-01-22 22:22:43 -0300

    Finalizando Aula 90.Básico sobre Array

--- array/array_basico.php
+++ array/array_basico.php
@@ -12,6 +12,38 @@
     $lista = array(1,2,3.4,"texto");
     echo $lista . '<br>';
     var_dump($lista);
+    echo '<br>';
+    echo $lista[0];
+    echo '<br>';
+    echo $lista[1];
+    echo '<br>';
+    echo $lista[2];
+    echo '<br>';
+    echo $lista[3];
+    echo '<br>';
+    echo $lista[4];
+    echo '<br>';
+    echo $lista[5];
+    echo '<br>';
+    echo $lista[15];
+    echo '<br>';
+    echo $lista[100];
+    var_dump($lista[100]);
+
+    echo '<br>';
+    $texto = 'Jhonatan está estudando PHP';
+    echo $texto[0];
+    echo $texto[1];
+    echo $texto[2];
+    echo $texto[3];
+    echo $texto[4];
+    echo $texto[5];
+    echo '<br>';
+   
+    // Essa função pega quantidade / posição de uma string . Pega a possição da palvra que começa + quantidade de caracter que vocÊ quer que pegue
+    echo mb_substr($texto,0,4);
+
+
 
 
     ?>
