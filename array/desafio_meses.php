5adedecfa0f18673d035e3183ab67b8ededa0d92
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-01-28 12:24:47 -0300

    Desafio concluído do Meses

--- array/desafio_meses.php
+++ array/desafio_meses.php
@@ -9,7 +9,53 @@
     <div class="title"> Desafios dos Meses </div>
     <hr><br>
     <?php
+    $meses = array(
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julho",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro"
+    );
+
+    echo '<br>';
+    print_r($meses);
+
+    $mesesUtizandoChave = array(
+        1 => "Janeiro",
+        2 => "Fevereiro",
+        3 => "Março",
+        4 => "Abril",
+        5 => "Maio",
+        6 => "Junho",
+        7 => "Julho",
+        8 => "Agosto",
+        9 => "Setembro",
+        10 => "Outubro",
+        11 => "Novembro",
+        12 => "Dezembro"
+    );
+
+    echo '<br>';         
+    echo '<br>';     
     
+    print_r($mesesUtizandoChave);
+  
+    echo '<br>';         
+    echo '<br>';     
+    
+    #Estou utilozando um for para mostrar todos os meus valores dentro do meu array 
+
+    for($i=0; $i<=count($mesesUtizandoChave); $i++){
+        echo $mesesUtizandoChave[$i];
+    }
+
 
     ?>
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
