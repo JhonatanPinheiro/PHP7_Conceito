1d8f3285cd9eeeaa0ee8ab2e99eedf9ade38e117
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-02-24 16:14:32 -0300

    Criando meu formulário (Desafio 2)

---
+++ estruturas_controles_II/desafio2_tabela.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head> </head>
+
+<body>
+    <div class="title"> Desafio 2 da Tabela </div>
+    <hr><br>
+    <center>
+        <form class="formulario" id="formulario" action="#" method="post">
+                <label class="texto-label"> Quantas Colunas: </label>
+                <input class="style-input" type="number" id="qtd-coluna" name="qtd-coluna" placeholder="Informe um valor">
+                <br>
+                <label class="texto-label"> Quantas Linhas: </label>
+                <input class="style-input" type="number" id="qtd-linha" name="qtd-linha" placeholder="Informe um valor">
+                <hr style="width: 25rem;">
+            <button class="botao-criar">Criar</button>
+        </form>
+    </center>
+    <?php
+
+
+
+    ?>
+
+
+
+
+
+
+
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+</body>
+
+</html>
+
+<style>
+    .formulario {
+        padding: 5px;
+    }
+
+    .texto-label {
+        font-size: 2rem;
+        font-weight: bold;
+    }
+
+    .style-input{
+        font-size: 1.6rem;
+        width: 14rem;
+    }
+
+    .botao-criar{
+       font-size: 1.8rem; 
+    }
+
+</style>
