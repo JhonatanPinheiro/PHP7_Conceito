e1d15bb49180c0b80423af3911b9db868d443f77
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-03-02 20:35:30 -0300

    Finalizando desafio

--- funcoes/desafio_palindromo.php
+++ funcoes/desafio_palindromo.php
@@ -8,9 +8,12 @@
     <hr><br>
 
     <form id='formulario' method="post">
-        <label for="nome-coisa">Nome | Coisa: </label>
-        <input name="nome-coisa" id="nome-coisa" type="text">
-
+        <label for="nome-coisaI">Nome I | Coisa I: </label>
+        <input name="nome-coisaI" id="nome-coisaI" type="text">
+        <br>
+        <label for="nome-coisaII">Nome II | Coisa II: </label>
+        <input name="nome-coisaII" id="nome-coisaII" type="text">
+        <br>
         <button type="submit">Consultar</button>
     </form>
 
@@ -20,30 +23,31 @@
     # Se eu inverte a ordem de trás para frente ficará ana
     # Nesse caso ela é uma palavra palindrama
 
-  
-    
-    
     function verifica_palindramo(){
-        $nome_coisa = $_POST['nome-coisa'];
-        $ultimoIndice = strlen($nome_coisa) - 1; 
+        $nome_coisaI = $_POST['nome-coisaI'];
+        $ultimoIndice = strlen($nome_coisaI) - 1; 
 
         for($i = 0; $i <= $ultimoIndice; $i++){
-            if($nome_coisa[$i] !== $nome_coisa[$ultimoIndice - $i]){
-                return "<center>Não é Palindramo => {$nome_coisa}</center>";
+            if($nome_coisaI[$i] !== $nome_coisaI[$ultimoIndice - $i]){
+                return "<center>Não é Palindramo => {$nome_coisaI}</center>";
             }
         }
-        return "<center>Sim é Palindramo => {$nome_coisa}</center>";
+        return "<center>Sim é Palindramo => {$nome_coisaI}</center>";
     }
 
     echo verifica_palindramo();
 
 
+    echo '<hr>';
+    echo '<center> <h3> Segunda maneira </h3> </center>';
 
-            
+    function palindromoSimples(){
+        $nome_coisaII = $_POST['nome-coisaII'];
 
+        return $nome_coisaII === strrev($nome_coisaII) ? "<center>Sim é Palindramo => {$nome_coisaII}</center>":"<center>Não é Palindramo => {$nome_coisaII}</center>";
+    }
 
-
-
+    echo palindromoSimples();
 
     ?>
     <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
