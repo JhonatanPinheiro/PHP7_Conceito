efb785cb5898270b0149cf68f69bfd147dedcd6a
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-03-10 14:18:31 -0300

    Finalizando a aula de Argumento Padrão

--- funcoes/argumentos_padroes.php
+++ funcoes/argumentos_padroes.php
@@ -7,7 +7,27 @@
     <div class="title">Argumentos Padrões </div>
     <hr><br>
     <?php
-     
+     function saudacao($nome = 'Senhor (a)', $sobrenome = 'Cliente'){
+        return "Bem vindo, $nome $sobrenome! <br>";
+     }
+
+     echo saudacao();
+     echo saudacao(NULL);
+     echo saudacao(NULL,NULL);
+     echo saudacao('Mestre', 'Supremo');
+
+     echo '<hr>';
+
+     function anotarPedido($comida, $bebida = 'Água'){
+        return "Para comer: $comida <br>"  . "Para beber: $bebida <br>";
+     }
+
+     echo anotarPedido('Hamburguer');
+
+
+
+     #DICA: Sempre que tivermos trabalhando com valores padrões é muito importante deixar eles passando por último. Pois dessa formar ao chamar-mos a função não precisamos ficar passando o valor das variáveis nos parâmetros
+
     ?>
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 </body>
