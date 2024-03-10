be19bda3037d98e3802803bce6b12f4b0dfaa0a8
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-03-10 13:58:41 -0300

    Aprendendo argumentos variaveis ...$array

--- funcoes/argumentos_variaveis.php
+++ funcoes/argumentos_variaveis.php
@@ -7,6 +7,55 @@
     <div class="title">Argumentos Variáveis </div>
     <hr><br>
     <?php
+        //Usando uma função dos dois números
+        function soma($a, $b){
+            return $a + $b;
+        }
+
+        echo soma(15,15);
+
+        echo '<hr>';
+
+        // Fazendo a função que eu posso passar quantos números que eu quiser que ele irá somar. 
+        function somaCompleta(...$numeros){
+            #Quando colamos 3 pontinhos ele pegar todos os valores e transforma em um Array.
+            #print_r($numeros);
+
+            $soma = 0;
+            foreach($numeros as $num){
+                $soma+=$num;
+            }
+            return $soma;
+        }
+
+
+        #echo somaCompleta(1,2,3,4,5,6,7,8,9,10);
+        echo somaCompleta(2,2,2,2,10);
+
+        echo '<hr>';
+
+        $array = [10,20,100];
+        echo somaCompleta(...$array);
+        echo '<hr>';
+
+        function membros($titular, ...$dependentes){
+            echo "Titular: $titular <br>";
+            if($dependentes){
+                foreach($dependentes as $dep){
+                    echo "Dependentes:  $dep <br>";
+                }
+            }
+        }
+
+
+        echo '<br>';
+
+        membros("Jhonatan", "teste1", "teste2");
+
+        membros("Layara");
+
+
+
     
     ?>
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
