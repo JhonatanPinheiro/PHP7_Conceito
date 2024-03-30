872ef80b1988c0f76aad793ddb81a8587dcf2cf1
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-03-30 14:26:45 -0300

    Aprendendo funções anônima

--- funcoes/funcoes_anonima.php
+++ funcoes/funcoes_anonima.php
@@ -7,10 +7,46 @@
     <div class="title">Função Anônima </div>
     <hr><br>
     <?php
-        
+
+    //função anônima 
+    $soma = function ($a, $b) {
+        return $a + $b;
+    };
+
+    echo $soma(1, 2, '<br>');
+    echo '<hr>';
+
+    function executar($a, $b, $op, $funcao)
+    {
+        $resultado = $funcao($a, $b) ?? NULL;
+
+        echo "$a $op $b = $resultado<br>";
+    }
+
+    executar(2, 3, '+', $soma);
+
+    echo '<hr>';
+
+    //função anônima 
+    $multiplicacao = function ($a, $b) {
+        return $a * $b;
+    };
+
+    executar(2, 3, '*', $multiplicacao);
+
+    echo '<hr>';
+
+
+
+
+
+
+
+
     ?>
-<script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 </body>
+
 </html>
 
 <style></style>
