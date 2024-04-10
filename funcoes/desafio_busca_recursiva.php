db7c764ff2b250858e2b28dfa400b31ee58529f2
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-04-10 11:49:21 -0300

    Finalizando o desafio do busca recursiva

--- funcoes/desafio_busca_recursiva.php
+++ funcoes/desafio_busca_recursiva.php
@@ -7,6 +7,33 @@
     <div class="title"> Desafio Busca Recursiva </div>
     <hr><br>
     <?php
+        /*
+            $array = [1,2,[3,4,5],6[7,[8,9]],10];
+
+        */
+    	
+        $array = [1,2, [3,4,5], 6, [7, [8,9]],10];
+
+        function imprimirArray($array, $nivel = '>'){
+            foreach($array as $index  => $elemento){
+                if(is_array($elemento)){
+                    imprimirArray($elemento,$nivel . $nivel[0]);
+                }else{
+                    echo "$nivel $elemento <br>";
+                }
+            }
+        };
+
+
+
+        imprimirArray($array);
+
+
+
+
+
+
+     
 
     ?>
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
