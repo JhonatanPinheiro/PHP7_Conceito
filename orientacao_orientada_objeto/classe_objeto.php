aa0a022b5fddda2f7e5e8def398b57fd4161593e
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-04-11 21:28:00 -0300

    Aprendendo sobre Classe e Objeto

--- orientacao_orientada_objeto/classe_objeto.php
+++ orientacao_orientada_objeto/classe_objeto.php
@@ -7,7 +7,33 @@
     <div class="title">  </div>
     <hr><br>
     <?php
-    echo 'teste';
+    
+  
+
+    echo "<p><strong>CLASSE (class):</strong> Em PHP, uma classe é uma estrutura que define um tipo de objeto. Ela serve como um modelo para criar objetos que compartilham características e comportamentos comuns. As classes podem conter propriedades (variáveis) e métodos (funções) que definem o estado e o comportamento dos objetos criados a partir delas.</p>";
+    echo "<p><strong>OBJETO:</strong> Em PHP, um objeto é uma instância de uma classe. Ou seja, quando você cria uma classe, você está definindo um novo tipo de objeto com suas próprias propriedades e métodos. Um objeto é então uma variável que representa uma instância específica dessa classe.</p>";
+    echo "<p><strong>ATRIBUTO:</strong> Em PHP, o termo 'atributo' é frequentemente usado de forma intercambiável com 'propriedade' para descrever as variáveis declaradas dentro de uma classe. Quando você define uma classe em PHP, você pode declarar variáveis dentro dela, que são chamadas de propriedades ou atributos da classe.<p/>";
+    echo "<p><strong> CONSTRUTOR: </strong> Um construtor em PHP é um método especial dentro de uma classe que é automaticamente chamado quando um objeto dessa classe é instanciado, ou seja, quando você usa a palavra-chave new para criar uma nova instância da classe.O principal propósito de um construtor é inicializar o objeto com valores específicos ou realizar outras tarefas de inicialização necessárias antes que o objeto seja utilizado.Em PHP, o construtor é definido dentro de uma classe usando um método chamado __construct(). Aqui está um exemplo simples de uma classe Pessoa com um construtor</p>";
+    echo "<p><strong> MÉTODO: </strong> Em PHP, um método é uma função que está associada a uma classe específica. Métodos são definidos dentro de uma classe e são usados para definir o comportamento dos objetos dessa classe. Eles são responsáveis por realizar operações específicas nos dados do objeto, geralmente manipulando as propriedades do objeto ou realizando cálculos com esses dados.</p>";
+    echo "<p><strong> DESTRUTOR: </strong> Um destrutor em PHP é um método especial dentro de uma classe que é automaticamente chamado quando um objeto dessa classe é destruído ou quando o script termina sua execução. Enquanto um construtor é usado para inicializar um objeto quando ele é criado, o destrutor é usado para realizar ações de limpeza ou liberação de recursos quando o objeto não é mais necessário. Em PHP, um destrutor é definido dentro de uma classe usando o método __destruct(). Este método não aceita parâmetros e é chamado </p>";
+   
+    /*
+    ________________________
+            CLASSE 
+    ________________________
+        VARIAVEIS/CONSTANTE 
+        CONSTRUTOR
+        MÉTODOS 
+        DESTRUTOR
+    _________________________
+    
+    */
+
+    
+
+
+
+
 
     ?>
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
