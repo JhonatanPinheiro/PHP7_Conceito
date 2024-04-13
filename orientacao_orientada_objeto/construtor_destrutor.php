df9bf0bc1c42542226b8ed1af340ac4e202f0835
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-04-13 14:31:09 -0300

    Atualizando as construtor e Desconstrutor

--- orientacao_orientada_objeto/construtor_destrutor.php
+++ orientacao_orientada_objeto/construtor_destrutor.php
@@ -4,14 +4,42 @@
 <head> </head>
 
 <body>
-    <div class="title"> Construtor e Destrutor  </div>
+    <div class="title"> Construtor e Destrutor </div>
     <hr><br>
     <?php
+    class Pessoa {
+        public  $nome;
+        public  $idade;
+        public  $cpf;
+        public  $sexo;
 
-       
-   ?>
-<script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+        public function __construct($novoNome, $novaIdade, $novoSexo){
+                $this -> nome = $novoNome;
+                $this -> idade = $novaIdade;
+                $this -> sexo = $novoSexo;
+        }
+
+        public function __destruct(){
+            return "Morreu!!";
+        }
+
+        public function apresentar(){
+             return "<br> Nome: {$this->nome} <br> Idade: {$this -> idade} <br> Sexo : {$this -> sexo}";
+        }
+
+    }
+
+    $p1 = new Pessoa('Japa', '25', 'Masculino');
+    echo $p1 -> apresentar();
+
+    $p2 = new Pessoa('Julio', '28', 'F');
+    echo $p2 -> apresentar();
+
+
+    ?>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 </body>
+
 </html>
 
 <style></style>
