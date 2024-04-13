9e7b2f447936ddd8acf908bed6750162618a6a04
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-04-12 22:22:31 -0300

    Criando o arquivo que iremos iniciar sobre o assunto de Construtor e Destrutor

--- index.php
+++ index.php
@@ -125,6 +125,7 @@
                     <li><a href="exercicio.php?dir=orientacao_orientada_objeto&file=classe_objeto"> Classe Vs Objeto </a></li>
                     <li><a href="exercicio.php?dir=orientacao_orientada_objeto&file=classe"> Classe </a></li>
                     <li><a href="exercicio.php?dir=orientacao_orientada_objeto&file=desafio_classe_data"> Desafio Classe Data </a></li>
+                    <li><a href="exercicio.php?dir=orientacao_orientada_objeto&file=construtor_destrutor"> Construtor e Destrutor </a></li>
                     <li><a href="#">Exercício B</a></li>
                     <li><a href="#">Exercício C</a></li>
                 </ul>
