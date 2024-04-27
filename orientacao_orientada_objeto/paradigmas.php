6990e007542571ae4fb217a899537d48971d20fa
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-04-27 14:04:20 -0300

    TEORIA DO PARADIGMAS

---
+++ orientacao_orientada_objeto/paradigmas.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head> </head>
+
+<body>
+    <div class="title"> Paradigmas </div>
+    <hr><br>
+    <?php
+   
+    ##Não estruturado (Goto)
+    ##Procedural
+    ##Funcional
+    ##Orientação a Objeto
+
+    ##PROCEDURAL -> ATOR PRINCIPAL É A FUNÇÃO
+          #vs
+    ##ORIENTAÇÃO A OBJETOS -> ATOR PRINCIPAL É O OBJETO
+    
+    ?>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+</body>
+
+</html>
+
+<style></style>
