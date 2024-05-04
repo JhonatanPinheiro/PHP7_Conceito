552c92e200a5cb6e2e83f4dd7dbfca82a89f377f
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-05-04 20:47:18 -0300

    Finalizando a aula de membros de classe

--- orientacao_orientada_objeto/membros_de_classe.php
+++ orientacao_orientada_objeto/membros_de_classe.php
@@ -19,14 +19,24 @@
             echo "Não Estático =  {self::$static} <br>";
             #Tentativa 3
             echo "Não Estático = " . self::$static . "<br>";
-
-
         }
+
+        public static function mostrarStaticA(){
+           # echo "Não Estática = {$this->naoStatic}<br>";
+            echo "Estática =". self::$static. "<br>";
+        }
+    
     }
+
 
     $obejtoA = new A();
     $obejtoA->mostrarA();
+    #$obejtoA->mostrarStaticA(); #Não é a forma ideal
+    A::mostrarStaticA(); #Forma mais adequada de acessar uma função Statica através da classe
+    echo A::$static .  '<br>'; // Acessa diretamente a Classe
 
+    A::$static = 'Valor alterado do membro de classe!';
+    echo A::$static;
 
 
     ?>
