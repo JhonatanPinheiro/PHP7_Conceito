d8d7a5bb6d7dd19f68ea8817652961023fb4a887
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-06-03 21:40:13 -0300

    Finalizando a atividade de traits

--- orientacao_orientada_objeto/traits_01.php
+++ orientacao_orientada_objeto/traits_01.php
@@ -9,6 +9,7 @@
     <?php
      trait validacao{
         #Aqui dentro definimos métodos
+        public $a = 'Valor a';
         public function validarString($str){
             return isset($str) && $str !=='';
         }
@@ -16,6 +17,9 @@
 
     #Um trait é uma forma de reutilizar código em várias classes de forma horizontal, ou seja, em classes que não estão relacionadas por meio de herança.
     trait validacaoMelhor {
+        public $b = 'Valor b';
+        private $c = 'Valor privado';
+
         public function validarStringMelhor($str){
             return isset($str) && trim($str);
         }
@@ -24,12 +28,21 @@
     #Não usamos uma trait a partir de uma herança. Usamos uma traits com a palavra use
     class Usuario{
         use validacao,validacaoMelhor;
+
+        public function imprimirValorC(){
+            echo '<br>',$this->c;
+        }
     }
 
     #var_dump(validacao->validarString);
     $usuario = new Usuario();
     var_dump($usuario->validarString(' '));
     echo '<br>';
+    var_dump($usuario->validarStringMelhor(' '));
+    echo '<br>';
+    echo $usuario->a, '<br>' ,$usuario->b;
+    echo '<br>';
+    echo $usuario->imprimirValorC();
 
     ?>
     <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
