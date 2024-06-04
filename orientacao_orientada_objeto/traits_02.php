7babbdd492456952ee72691be8e5a4d2172ba272
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-06-03 22:49:34 -0300

    Parte 2 do Traits_02

--- orientacao_orientada_objeto/traits_02.php
+++ orientacao_orientada_objeto/traits_02.php
@@ -7,7 +7,31 @@
     <div class="title"> TRAITS 02 </div>
     <hr><br>
     <?php
-   
+        trait validacao {
+            public function validarString($str) {
+                return isset($str) && $str !== '';
+            }
+        }
+
+        trait validacaoMelhor {
+            public function validarString($str) {
+                return isset($str) && trim($str);
+            }
+        }
+
+        class Usuario {
+            use validacao, validacaoMelhor {
+                validacaoMelhor::validarString insteadOf validacao;
+                // validacao::validarString insteadOf validacaoMelhor;
+
+                validacao::validarString as validacaoSimples;
+            }
+        }
+
+        $usuario = new Usuario();
+        var_dump($usuario->validarString(' '));
+        echo '<br>';
+        var_dump($usuario->validacaoSimples(' '));
 
     ?>
     <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
