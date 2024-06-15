7d54cdbc1da3994524445c6e02214e3402eea8c3
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-06-15 06:51:40 -0300

    Adicionando o arquivo de método mágico

---
+++ orientacao_orientada_objeto/metodo_magico.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head> </head>
+
+<body>
+    <div class="title"> Métodos Mágicos </div>
+    <hr><br>
+    <?php
+      
+
+    ?>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+</body>
+
+</html>
+
+<style></style>
