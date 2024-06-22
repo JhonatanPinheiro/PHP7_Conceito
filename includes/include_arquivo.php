5d6b653874864119b2133be88cebd1bee813df69
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-06-22 14:59:34 -0300

    Entendo o include

--- includes/include_arquivo.php
+++ includes/include_arquivo.php
@@ -3,8 +3,10 @@
 
     $variavel = 'Estou definida';
 
-    function soma($a, $b) {
-        return $a + $b;
+    if(!function_exists('soma')){
+        function soma($a, $b) {
+            return $a + $b;
+        }
     }
-    
+ 
 ?>
