3b8c2cfb309aa552e7331355fb471f7a0e0a1c49
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-07-13 02:09:19 -0300

    useas e suas finalidades

--- namespace/useas.php
+++ namespace/useas.php
@@ -1,4 +1,33 @@
-<div class="titulo"> Use / As </div>
+<?php namespace Outro\Nome;?>
+<div class="titulo"> USE/AS </div>
 <?php
+echo __NAMESPACE__ . '<br>';
+
+include('useas_arquivo.php');
+
+function soma($a,$b){
+    return $a . $b; #Está errado, será apenas para diferenciar 
+ }
+
+class Classe {
+    public $var;
+    function func() {
+        echo __NAMESPACE__ .'->' . __CLASS__ . '->' . __METHOD__ . 'br' ;
+    }
+}
+
+#Posso fazer dessa maneira passando o caminho e o NAMESPACE
+echo \Nome\Bem\Grande\constante . '<br>';
+
+#Posso fazer dessa maneira usando o USE
+use const \Nome\Bem\Grande\constante;
+echo constante . '<br>';
+
+#Posso fazer de uma segunda maneira usando o USE porém irei utilizar Alias. 
+use Nome\Bem\Grande AS ctx;
+echo soma(1,2). '<br>'; # Porém estarei referenciando o função desse mesmo NAMESPACE 'Outro\Nome' - Nesse próprio arquivo 'useas.php'
+echo ctx\soma(1,2).'<br>'; #Aqui sim estárei referencia o NAMESPACE do meu outro arquivo 'useas_arquivo' com seu NAMESPACE 'Nome\Bem\Grande'.
+
+
 
 ?>
