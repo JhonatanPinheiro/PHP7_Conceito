92faa861cda810bed1aa0e16209bc9c4413fa5ca
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-07-22 20:55:28 -0300

    Aprendo sobre funções para tratamento de erros

--- tratamento_de_erro/error_handler.php
+++ tratamento_de_erro/error_handler.php
@@ -25,11 +25,13 @@
         return !!stripos(" $erroString", $text);
     }
    
-    set_error_handler('filtrarMensagem', E_WARNING);
+    set_error_handler('filtrarMensagem', E_WARNING); #A função set_error_handler() permite que você defina uma função personalizada para lidar com erros PHP padrão, como erros de aviso (E_WARNING), erros de parse (E_PARSE), e outros tipos de erros que podem ocorrer durante a execução do seu código
 
     echo '<hr>';
     echo  4 / 0 . '<br>';
     include 'arquivo_inexistente.php';
+
+    restore_error_handler(); #A função restore_error_handler() em PHP é utilizada para restaurar o manipulador de erros padrão do PHP após ter sido alterado por set_error_handler(). Aqui está uma explicação clara e objetiva sobre como essa função funciona:
     ?>
     <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 </body>
