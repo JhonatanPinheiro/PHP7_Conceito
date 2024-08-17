db7bc93e416254dd41e039e72791dbaccf1d0e4e
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-08-17 02:00:57 -0300

    Finalizando a configuração do banco de dados via php

--- db/conexao.php
+++ db/conexao.php
@@ -1,16 +1,15 @@
 <?php
     function novaConexao($banco = 'curso_php'){
-        $servidor = 'localhost:3307';
+        $servidor = '127.0.0.1:3307';
         $usuario = 'root';
         $senha = 'root';
 
-        $conexao = new mysqli($servidor,$usuario,$senha,$banco);
+        $conexao = new mysqli($servidor, $usuario, $senha, $banco);
+  
+        if($conexao->connect_error){
+            die('Erro: ' . $conexao->connect_error);
+        }
+        
+        return $conexao;
     }
-
-    if($conexao->connect_error){
-        die('Erro: ' . $conexao->connect_error);
-    }
-
-    return $conexao;
-
 ?>
--- db/criar_database.php
+++ db/criar_database.php
@@ -6,9 +6,23 @@
 
 <body>
     <div class="title"> Formulário - Cadastro </div>
-    <br>
-    <hr>
+    <br><hr>
+    <?php
+        require_once "conexao.php";
+      
+        $conexao = novaConexao(NULL);
+        $sql = 'CREATE DATABASE IF NOT EXISTS curso_php';
 
+        $resultado = $conexao->query($sql);
+
+        if($resultado){
+            echo "Sucesso!";
+        }else{
+            echo "Error!" . $conexao->error;
+        }
+
+        $conexao->close();
+    ?>
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
