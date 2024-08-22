5cc47573d1de427ce9ddc57b5ce7935d14d674ab
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-08-21 21:02:35 -0300

    Mostrando informações na tela em array

--- db/consultar.php
+++ db/consultar.php
@@ -8,9 +8,31 @@
     <div class="title"> SELECT - Consultar Dados no Banco de Dados  </div>
     <br><hr>
     <?php
-     
+        require_once('conexao.php');
 
+        $sql = "SELECT * FROM cadastro";
+
+        $conexao = novaConexao();
+
+        $resultadoTabelaCadastro = $conexao->query($sql);
+
+        //Mostrando na Tela
+        $registros = [];
+
+        if($resultadoTabelaCadastro->num_rows > 0){
+            while($row = $resultadoTabelaCadastro->fetch_assoc()){
+                #Esse fetch_assoc ele é um array associativo. Chave e Valor
+                $registros[] = $row;
+            }
+
+        }elseif($conexao->error){
+            echo "Erro: " . $conexao->error;
+        }
+
+        print_r($registros);
+        $conexao->close();
     ?>
+
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
