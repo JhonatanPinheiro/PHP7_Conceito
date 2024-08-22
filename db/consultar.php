aec2913cc179ef9ace196b9afe57e15673e7d771
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-08-21 22:25:09 -0300

    Ajustando o código com o commit correto!

--- db/consultar.php
+++ db/consultar.php
@@ -29,13 +29,48 @@
             echo "Erro: " . $conexao->error;
         }
 
-        print_r($registros);
         $conexao->close();
     ?>
 
+    <table class="table table-hover table-striped table-bordered">
+        <thead>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Nascimento</th>
+            <th>Email</th>
+            <th>Site</th>
+            <th>Qtd_filhos</th>
+            <th>Salario</th>
+            <th>Login</th>
+            <th>Ip</th>
+        </thead>
+        <tbody>
+            <?php foreach($registros as $registro):?>
+                <tr>
+                    <td><?= $registro['id'] ?></td>  
+                    <td><?= $registro['nome'] ?></td>
+
+                    <!-- Estou formatando a date para formato br DD/MM/YYYY, usando strtotime -->
+                    <td>
+                        <?= DATE('d/m/Y', strtotime($registro['nascimento'])) ?>
+                    </td>  
+                  
+                    <td><?= $registro['email'] ?></td>  
+                    <td><?= $registro['site'] ?></td>  
+                    <td><?= $registro['qtd_filhos'] ?></td>  
+                    <td><?= $registro['salario'] ?></td>  
+                    <td><?= $registro['login'] ?></td>  
+                    <td><?= $registro['ip'] ?></td>  
+                </tr>
+            <?php endforeach?>
+        </tbody>
+    </table>
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
-
-<style></style>
+<style>
+    table > * {
+        font-size: 1.0rem;
+    }
+</style>
