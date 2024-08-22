acb8b4b7d3dbe7f2d6dd81988b67b556dc0c4172
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-08-21 22:38:05 -0300

    Adicionando o arquivos para deletar os registros dentro da base de dados

--- db/excluir_model1.php
+++ db/excluir_model1.php
@@ -10,7 +10,16 @@
     <?php
         require_once "conexao.php";
 
-       
+        $sql = "DELETE FROM cadastro WHERE id IN(10,11,12,13,14,15)";
+
+        $conexao = novaConexao();
+        $resultadoDeleteDados = $conexao->query($sql);
+
+        if($resultadoDeleteDados){
+            echo "Registro foi excluído do Banco de Dados";
+        }else{
+            echo "Error: " . $conexao->error;
+        }      
     ?>
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
