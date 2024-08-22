70fa0b3a6a74ac4177318d5c68cf54b1dd7664d0
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-08-22 17:59:12 -0300

    Adicionando arquivo alterar

--- menu.php
+++ menu.php
@@ -188,6 +188,7 @@
             <li><a href="exercicio.php?dir=db&file=excluir_model1"> Excluir registros no Banco de Dados #01 </a></li>
             <li><a href="exercicio.php?dir=db&file=excluir_model2"> Excluir registros no Banco de Dados #02 </a></li>
             <li><a href="exercicio.php?dir=db&file=consultar"> SELECT - Consultar Dados no Banco de Dados </a></li>
+            <li><a href="exercicio.php?dir=db&file=alterar"> Alterar - Dados no Banco de Dados </a></li>
             <li><a href="exercicio.php?dir=db&file=migrate"> Migrate </a></li>
         </ul>
     </div>
