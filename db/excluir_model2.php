6233ae8db55096617ee3d06872f6534eb48a5097
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-08-21 23:57:07 -0300

    Criando um botão de excluir dados específicos pelo id

--- db/excluir_model2.php
+++ db/excluir_model2.php
@@ -10,20 +10,78 @@
     <?php
         require_once "conexao.php";
 
-        $sql = "DELETE FROM cadastro WHERE id IN(10,11,12,13,14,15)";
-
+        $registros = [];
         $conexao = novaConexao();
-        $resultadoDeleteDados = $conexao->query($sql);
 
-        if($resultadoDeleteDados){
-            echo "Registro foi excluído do Banco de Dados";
-        }else{
-            echo "Error: " . $conexao->error;
-        }      
+        if($_GET['excluir']){
+            $excluirSQL = "DELETE FROM cadastro WHERE id = ?";
+            #Ele vai ler esse valor e não vai deixar um valor inválido seja enviado. Impedindo que tenho problema de ataque SQL INJECÇÂO
+            $stmt = $conexao->prepare($excluirSQL);
+            $stmt->bind_param("i",$_GET['excluir']);
+            $stmt->execute();
+        }
+    
+        $sql = "SELECT * FROM cadastro";
+        $resultado = $conexao->query($sql);
+
+        if($resultado->num_rows > 0){
+            while($row = $resultado->fetch_assoc()){
+                $registros[] = $row;
+            }
+        }elseif($conexao->error){
+            echo 'Error: '. $conexao->error;
+        }
+
+        $conexao->close();
     ?>
+
+    <table class="table table-hover table-striped table-bordered">
+        <thead>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Nascimento</th>
+            <th>Email</th>
+            <th>Site</th>
+            <th>Qtd_filhos</th>
+            <th>Salario</th>
+            <th>Login</th>
+            <th>Ip</th>
+            <th>Ações</th>
+        </thead>
+        <tbody>
+            <?php foreach($registros as $registro):?>
+                <tr>
+                    <td><?= $registro['id'] ?></td>  
+                    <td><?= $registro['nome'] ?></td>
+
+                    <!-- Estou formatando a date para formato br DD/MM/YYYY, usando strtotime -->
+                    <td>
+                        <?= DATE('d/m/Y', strtotime($registro['nascimento'])) ?>
+                    </td>  
+                  
+                    <td><?= $registro['email'] ?></td>  
+                    <td><?= $registro['site'] ?></td>  
+                    <td><?= $registro['qtd_filhos'] ?></td>  
+                    <td><?= $registro['salario'] ?></td>  
+                    <td><?= $registro['login'] ?></td>  
+                    <td><?= $registro['ip'] ?></td>  
+                    <td>
+                        <a href="/exercicio.php?dir=db&file=excluir_model2&excluir=<?= $registro['id'] ?>" class="btn btn-danger"> Excluir </a>
+                    </td>  
+                </tr>
+            <?php endforeach?>
+        </tbody>
+    </table>
+
+
+
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
+<style>
+    table > * {
+        font-size: 1rem;
+    }
 
-<style></style>
+</style>
