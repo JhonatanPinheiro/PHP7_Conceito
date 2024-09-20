11cc47e1acf32990b3b0a64bcacbf431b165f564
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-09-20 20:22:44 -0300

    Adicionando nova funcionalidade de alteração de dados

--- menu.php
+++ menu.php
@@ -189,6 +189,7 @@
             <li><a href="exercicio.php?dir=db&file=excluir_model2"> Excluir registros no Banco de Dados #02 </a></li>
             <li><a href="exercicio.php?dir=db&file=consultar"> SELECT - Consultar Dados no Banco de Dados </a></li>
             <li><a href="exercicio.php?dir=db&file=alterar"> Alterar - Dados no Banco de Dados </a></li>
+            <li><a href="exercicio.php?dir=db&file=alterarTeste"> Alterar Teste - Dados no Banco de Dados </a></li>
             <li><a href="exercicio.php?dir=db&file=migrate"> Migrate </a></li>
         </ul>
     </div>
