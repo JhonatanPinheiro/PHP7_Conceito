f9d4de0c1d79346aae9c7b338b0d1541a95f94ab
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2024-09-21 13:24:07 -0300

    Adicionando o arquivo de conexao_pdo utilizando PDO

---
+++ db/conexao_pdo.php
@@ -0,0 +1,21 @@
+<?php
+
+function novaConexao($banco = 'curso.php'){
+    $servidor = 'localhost:3306';
+    $usuario = 'root';
+    $senha = 'root';
+
+    try{
+        $conexao = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
+        #1 Paramentro: Informar o tipo do banco de dados
+        #2 Paramentro: Informar o localhost + porta
+        #3 Paramentro: usuario
+        #4 Paramentro: senha
+
+        return $conexao;
+    }catch(PDOException $e){
+        die('Error:' . $e->getMessage());
+    }
+}
+
+?>
